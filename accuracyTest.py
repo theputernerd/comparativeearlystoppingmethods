21d998fb370df41fd4ee61891fef4cadd64884af
theputernerd <benson74>
2018-08-09 11:30:06 +1000

    3000 games

--- accuracyTest.py
+++ accuracyTest.py
@@ -765,7 +765,7 @@ def coverageTest(ngames=5000, epsilon=0.05, alpha=0.05, pmargin=0.5):
     bpredictiongrid['0.5-epsilon<pab and pab<0.5'] = np.zeros(3)
     bpredictiongrid['0.5<pab and pab<=0.5+epsilon'] = np.zeros(3)
     bpredictiongrid['pab>0.5+epsilon'] = np.zeros(3)
-    s=np.arange(0.44,0.9,0.01)
+    s=np.arange(0.43,0.57,0.01)
     #s=[0.5]
     for p in s:
         p=np.round(p,3) #without this python stores p=0.45 as 0.4499999999 which is not a draw value!!!!. unfair.
@@ -1036,7 +1036,7 @@ if __name__ == '__main__':
     alpha=0.05
     epsilon=0.05
     coverageTest(ngames=100, epsilon=epsilon, alpha=alpha, pmargin=0.5)
-    choosefromPoolTest(ngames=1000, epsilon=epsilon, alpha=alpha, pmargin=0.5)
+    choosefromPoolTest(ngames=2000, epsilon=epsilon, alpha=alpha, pmargin=0.5)
 
 
 
